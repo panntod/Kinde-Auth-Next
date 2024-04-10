2f90cbc834dec5e802b4c1ac6b1c96e43bd391c5
panntod <pandhu.munjalindra@gmail.com>
2024-04-10 09:28:06 +0700

    feat: Create dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function page() {
+    return (
+        <section className="bg-gray-900 text-white rounded-xl p-5 w-4/5 flex flex-col justify-center items-center gap-8 mx-auto grow mb-10 text-center">
+            <p className="text-2xl font-bold">Boom!</p>
+            <h1 className="text-6xl font-semibold">Your Auth is all sorted.</h1>
+            <p className="text-3xl font-bold">Build the important stuff.</p>
+        </section>
+    )
+}
